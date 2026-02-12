159f9cd1210ddbe7f5452e7436d749e60ccc2dfe
SayNyaOrDie <90498305+SayNyaOrDie@users.noreply.github.com>
2026-02-12 17:44:21 +0300

    Add files via upload

--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,30 @@
-// @todo: Темплейт карточки
+const cardTemplate = document.querySelector('#card-template').content;
 
-// @todo: DOM узлы
+const placesList = document.querySelector('.places__list');
 
-// @todo: Функция создания карточки
+function deleteCard(cardElement) {
+  cardElement.remove();
+}
 
-// @todo: Функция удаления карточки
+function createCard(cardData, deleteCallback) {
+  const cardElement = cardTemplate.querySelector('.card').cloneNode(true);
 
-// @todo: Вывести карточки на страницу
+  const cardImage = cardElement.querySelector('.card__image');
+  const cardTitle = cardElement.querySelector('.card__title');
+  const deleteButton = cardElement.querySelector('.card__delete-button');
+
+  cardImage.src = cardData.link;
+  cardImage.alt = cardData.name;
+  cardTitle.textContent = cardData.name;
+
+  deleteButton.addEventListener('click', function () {
+    deleteCallback(cardElement);
+  });
+
+  return cardElement;
+}
+
+initialCards.forEach(function (cardData) {
+  const cardElement = createCard(cardData, deleteCard);
+  placesList.append(cardElement);
+});
